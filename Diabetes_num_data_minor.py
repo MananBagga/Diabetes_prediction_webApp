5496aba9cd2517c795516926fa2f8b55a15013b9
Manan Bagga <Mananbagga0310@gmail.com>
2024-09-19 01:31:48 +0530

    Update Diabetes_num_data_minor.py

--- Diabetes_num_data_minor.py
+++ Diabetes_num_data_minor.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model = pickle.load(open('Diabetes_prediction_webApp/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 def diabetes_prediction(input_data):
     
